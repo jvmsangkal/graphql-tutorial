e196476547ed88114af1d5f6d528c1a59a33033b
John Viscel Sangkal <jvmsangkal@gmail.com>
2020-05-29 15:38:57 +0800

    exercise on mutations

--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => links.find((link) => link.id === args.id)
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+
+      links.push(link)
+
+      return link
+    },
+    updateLink: (parent, {id, url, description}) => {
+      let link = links.find((link) => link.id === id)
+
+      if (!link) {
+        return
+      }
+
+      Object.assign(link, { url, description })
+
+      return link
+    },
+    deleteLink: (parent, { id }) => {
+      let link = links.find((link) => link.id === id)
+
+      links = links.filter(link => link.id !== id)
+      return link
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 
