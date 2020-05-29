2ffcf9790919bd324a2ba026d43c72e4e8dffe25
John Viscel Sangkal <jvmsangkal@gmail.com>
2020-05-30 00:20:24 +0800

    implement filtering

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,31 @@
 const info = () => `This is the API of a Hackernews Clone`
 
 const feed = (root, args, context) => {
-  return context.prisma.links()
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  })
+
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count()
+
+  return {
+    links,
+    count,
+  }
 }
 
 const link = (root, args, context) => {
